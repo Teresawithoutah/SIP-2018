a4f360d7f8c5e6622ded5c50b74494354caba42c
Teresa <teresawithoutah@gmail.com>
2019-01-09 22:21:22 -0500

    files from gwc

--- U1-Fundamentals/U1.2-Python/AllTheFilters/part4/filtergram.py
+++ U1-Fundamentals/U1.2-Python/AllTheFilters/part4/filtergram.py
@@ -1,28 +1,22 @@
 import filters
 
+
 def main():
   # Ask what image the user wants to edit
   filename = input("Enter the name of the image file to edit: ")
-  
+
   # Load the image from the specified file
-  img = filters.load_img(filename)
+  im = filters.load_img(filename)
 
-  # Apply filters!
-  newimg = filters.obamicon(img)
-  newimg = filters.grayscale(newimg)
+  # # Apply filters!
+  # newimg = filters.obamicon(img)
 
-  blue = (30,85,115)
-  anotherimg = filters.emphasize(img, blue, 50)
+  im.show()
 
-  blueimg = filters.add_color(img, blue)
 
-  lastimg = filters.invert(blueimg)
 
-  # Save the final images
-  filters.save_img(newimg, "recolored1.jpg")
-  filters.save_img(anotherimg, "recolored2.jpg")
-  filters.save_img(blueimg, "recolored3.jpg")
-  filters.save_img(lastimg, "recolored4.jpg")
-
+  # # Save the final image
+  # filters.save_img(newimg, "recolored.jpg")
+  #
 if __name__ == '__main__':
   main()
